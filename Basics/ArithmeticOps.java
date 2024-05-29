071c12d9fef15e78e84bc19d1cefbd3602a942b0
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-29 12:40:06 +0530

    Create ArithmeticOps.java

---
+++ Basics/ArithmeticOps.java
@@ -0,0 +1,18 @@
+package thirdProject;
+
+class ArithmeticOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 5;
+		int sum = a+b;
+		int sub = a-b;
+		int mul = a*b;
+		double div = a/b;
+		System.out.println("Addition is "+sum);
+		System.out.println("Subtration is "+sub);
+		System.out.println("Multiplication is "+mul);
+		System.out.println("Division is "+div);
+	}
+
+}
