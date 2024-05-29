1f4acaee288f6535d50c59d7b74d1123ecdb1be4
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-29 12:30:10 +0530

    Create Variable.java

---
+++ Basics/Variable.java
@@ -0,0 +1,37 @@
+package secondProject;
+
+public class B{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Second Project!!!");
+		
+		byte a = 26;
+		System.out.println(a);
+		
+		int b = 22039;
+		System.out.println(b);
+		
+		short c = 2000;
+		System.out.println(c);
+		
+		long d = 34345435636478L;
+		System.out.println(d);
+		
+		float e = 3342.43f;
+		System.out.println(e);
+		
+		double f = 1332;
+		System.out.println(f);
+		
+		char g = 'v';
+	    System.out.println(g);
+	    
+	    String h = "Vasudev";
+	    System.out.println(h);
+	    
+	    boolean i = true;
+	    System.out.println(i);
+	}
+
+}
