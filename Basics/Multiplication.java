759914c568a4fe54100d1f633f6edb250208e465
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-29 12:39:24 +0530

    Create Multiplication.java

---
+++ Basics/Multiplication.java
@@ -0,0 +1,11 @@
+package thirdProject;
+
+class Multiplication {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int mul = a*b;
+		System.out.println("Multiplication is " + mul);
+	}
+
+}
