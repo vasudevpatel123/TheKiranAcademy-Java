10d64758aaa50799a443d5a6ceda0246de3af275
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-29 12:38:35 +0530

    Create Addition.java

---
+++ Basics/Addition.java
@@ -0,0 +1,17 @@
+package thirdProject;
+
+class Addition {
+
+	public static void main(String[] args) {
+
+		int a = 10; 
+		int b = 5;
+		int sum = a+b;
+		System.out.println("Additon is "+sum);
+		
+		double avg = sum/2.0;
+		System.out.println("Average is "+avg);
+
+	}
+
+}
