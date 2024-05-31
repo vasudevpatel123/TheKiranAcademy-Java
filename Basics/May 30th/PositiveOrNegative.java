833dac8a5273dff5e77a1464e1565fc436f47ba1
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-31 15:53:42 +0530

    Create PositiveOrNegative.java

---
+++ Basics/May 30th/PositiveOrNegative.java
@@ -0,0 +1,22 @@
+package com.vasudev.may30;
+
+class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		
+		int a = 0;
+		if(a==0) {
+			System.out.println("Number "+a+
+					" is Zero.");
+		}else {
+			if(a>0)
+				System.out.println("Number "+a+
+						" is Positive.");
+			else
+				System.out.println("Number "+a+
+						" is Negative");
+		}
+
+	}
+
+}
