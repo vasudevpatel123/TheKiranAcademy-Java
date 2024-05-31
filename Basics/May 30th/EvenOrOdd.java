b74c05ebb4c1bbe15e6a7396c5cff289cc0347b7
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-05-31 15:51:23 +0530

    Create EvenOrOdd.java

---
+++ Basics/May 30th/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package com.vasudev.may30;
+
+class EvenOrOdd {
+	public static void main(String[] args) {
+		int a=41;
+		if(a%2==0) {
+			System.out.println("Number "+a+
+					" is Even number.");
+		}else {
+			System.out.println("Number "+a+
+					" is Odd number.");
+		}
+	}
+}
