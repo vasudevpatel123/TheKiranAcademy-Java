fbc5926dbe99fa168d992ca4d9e84844e28c3d7f
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-06-05 16:31:00 +0530

    Create Employee.java

---
+++ Basics/M2/June05/Employee.java
@@ -0,0 +1,18 @@
+package com.vasudev.m2.june5;
+
+public class Employee {
+
+	int id;
+	String name;
+	String designation;
+	int salary;
+	
+	void isEligibleForPromotion() {
+		System.out.println("Yes you are eligible for promotion");
+	}
+	
+	void isHavingCompanyLaptop() {
+		System.out.println("No, you have personal laptop and other systems.");
+	}
+	
+}
