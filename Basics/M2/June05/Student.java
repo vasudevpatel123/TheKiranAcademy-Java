e6a2db53f06564fc0eb21ad01197f4181084e76c
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-06-05 16:30:39 +0530

    Create Student.java

---
+++ Basics/M2/June05/Student.java
@@ -0,0 +1,17 @@
+package com.vasudev.m2.june5;
+
+public class Student {
+
+	int Id;
+	String name;
+	String course;
+	double percentage;
+	
+	void resultCheck() {
+		System.out.println("You have successfully passed the test!!!");
+	}
+	void isFootballPlayer() {
+		System.out.println("Yes you are enrolled in the Football game");
+	}
+	
+}
