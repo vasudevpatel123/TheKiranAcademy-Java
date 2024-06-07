983f0d133202f7500167d48abc66427da9721d87
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2024-06-07 14:35:15 +0530

    Create TestEmployee.java

---
+++ Basics/M2/June06/TestEmployee.java
@@ -0,0 +1,13 @@
+package com.vasudev.m2.june6;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee();
+		e1.accept(1, "Raju", 22, 25000);
+		e1.display();
+		
+	}
+	
+}
